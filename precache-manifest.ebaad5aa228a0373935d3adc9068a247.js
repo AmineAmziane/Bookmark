9a22f3bb07dea261cd6f0742221123a85fa440a2
AmineDz <amine2014max@gmail.com>
2020-07-31 17:10:44 +0100

    Updates

---
+++ precache-manifest.ebaad5aa228a0373935d3adc9068a247.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210ecb65cf29b9d0842be6a82c5f3a48",
+    "url": "/Bookmark/index.html"
+  },
+  {
+    "revision": "2f635525d2d5322e56b0",
+    "url": "/Bookmark/static/css/2.859ae28f.chunk.css"
+  },
+  {
+    "revision": "0993970d0162c4857082",
+    "url": "/Bookmark/static/css/main.ec6cb15a.chunk.css"
+  },
+  {
+    "revision": "2f635525d2d5322e56b0",
+    "url": "/Bookmark/static/js/2.f605456f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bookmark/static/js/2.f605456f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0993970d0162c4857082",
+    "url": "/Bookmark/static/js/main.f40dca69.chunk.js"
+  },
+  {
+    "revision": "9985f4c7ce182a9073a9",
+    "url": "/Bookmark/static/js/runtime-main.9fb0a213.js"
+  },
+  {
+    "revision": "7c66c472eb50c9abe027977b6f2b406e",
+    "url": "/Bookmark/static/media/bg-dots.7c66c472.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Bookmark/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Bookmark/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Bookmark/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Bookmark/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Bookmark/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "38993f94ba58120b39289a5258af7063",
+    "url": "/Bookmark/static/media/icon-arrow.38993f94.svg"
+  },
+  {
+    "revision": "ba45f6788c38e8a4fc487eab6847a365",
+    "url": "/Bookmark/static/media/icon-error.ba45f678.svg"
+  },
+  {
+    "revision": "c24c1904c0c62485f4200c3a49a45a27",
+    "url": "/Bookmark/static/media/icon-facebook.c24c1904.svg"
+  },
+  {
+    "revision": "a4daae10e6bd237b96908d16566647e2",
+    "url": "/Bookmark/static/media/icon-twitter.a4daae10.svg"
+  },
+  {
+    "revision": "25a237330ac7fab409f2b5833363ae97",
+    "url": "/Bookmark/static/media/illustration-features-tab-1.25a23733.svg"
+  },
+  {
+    "revision": "11b0ec333ed9d17690ada04979c489c0",
+    "url": "/Bookmark/static/media/illustration-features-tab-2.11b0ec33.svg"
+  },
+  {
+    "revision": "7f696303479b76b89e45fcf33b7aff4b",
+    "url": "/Bookmark/static/media/illustration-features-tab-3.7f696303.svg"
+  },
+  {
+    "revision": "dfa0cd821469019e297c8c27cfc0d475",
+    "url": "/Bookmark/static/media/illustration-hero.dfa0cd82.svg"
+  },
+  {
+    "revision": "53989d2a42baf707e1d92f5d05c7899b",
+    "url": "/Bookmark/static/media/logo-bookmark.53989d2a.svg"
+  },
+  {
+    "revision": "3e1ab00bc1a7182ca9297e73eaa98480",
+    "url": "/Bookmark/static/media/logo-chrome.3e1ab00b.svg"
+  },
+  {
+    "revision": "e5023c2b57490fa1d28da6dea398606b",
+    "url": "/Bookmark/static/media/logo-firefox.e5023c2b.svg"
+  },
+  {
+    "revision": "3d8f7bcc99ea84508c6a3380e0ae0878",
+    "url": "/Bookmark/static/media/logo-opera.3d8f7bcc.svg"
+  }
+]);
